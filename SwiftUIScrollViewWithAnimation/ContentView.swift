8071418ff2d94b58a8dfa5440a9ea39ec15711da
Jakub Slawecki <jakub.slawecki@hemmersbach.com>
2020-02-18 07:53:39 +0100

    hello world commit ;)

---
+++ SwiftUIScrollViewWithAnimation/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUIScrollViewWithAnimation
+//
+//  Created by Jakub Slawecki on 18/02/2020.
+//  Copyright © 2020 Jakub Slawecki. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
